5126d6411622bf753248cd31733ec1f224934530
Ryan <lifezqy@126.com>
2017-01-13 10:56:13 +0800

    recover bst

--- leetcode.cpp
+++ leetcode.cpp
@@ -1315,6 +1315,76 @@ class ValidateBinarySearchTree {
         }
 };
 
+class RecoverBinarySearchTree {
+    public:
+        void recoverTree(TreeNode *root) {
+
+            TreeNode* cur = 0;
+            TreeNode* pre = 0;
+            TreeNode* p1  = 0;
+            TreeNode* p2  = 0;
+            TreeNode* preCur = 0;
+
+            bool found = false;
+
+            if(!root) return;
+
+            cur = root;
+
+            while(cur){
+                if(!cur->left){
+                    //记录p1和p2
+                    if(preCur && preCur->val > cur->val){
+                        if(!found){
+                            p1 = preCur;
+                            found = true;
+                        }
+
+                        p2 = cur;
+                    }
+
+                    preCur = cur;
+                    cur = cur->right;
+
+                }else{
+
+                    pre = cur->left;
+                    while(pre->right && pre->right != cur){
+                        pre = pre->right;
+                    }
+
+                    if(!pre->right){
+
+                        pre->right = cur;
+                        cur = cur->left;
+                    }else{
+
+                        // 记录p1和p2
+                        if(preCur->val > cur->val){
+
+                            if(!found){
+                                p1 = preCur;
+                                found = true;
+                            }
+
+                            p2 = cur;
+                        }
+
+                        preCur = cur;
+                        pre->right = NULL;
+                        cur = cur->right;
+                    }
+                }
+            }
+
+            if(p1 && p2){
+                int t = p1->val;
+                p1->val = p2->val;
+                p2->val = t;
+            }
+        }
+};
+
 int main(){
 
     Solution s;
