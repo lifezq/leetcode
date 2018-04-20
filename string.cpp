2ebb465925fe745923c58d5dc26349ff7e2f4077
Ryan <lifezqy@126.com>
2018-04-20 16:29:40 +0800

    bit comparsion

--- string.cpp
+++ string.cpp
@@ -33,10 +33,45 @@ bool StringContain(string &a, string &b){
     return true;
 }
 
+bool StringContainPrime(string &a, string &b){
+    const int p[26] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101};
+    int f = 1;
+    for(unsigned int i=0; i<a.length(); ++i){
+        int x = p[a[i] - 'A'];
+        if(f%x){
+            f *=x;
+        }
+    }
+
+    for(unsigned int i=0;i<b.length();++i){
+        int x = p[b[i] - 'A'];
+        if(f%x){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool StringContainBit(string &a, string &b){
+    int hash = 0;
+    for(unsigned int i=0;i<a.length();++i){
+        hash |=(1<<(a[i]-'A'));
+    }
+
+    for(unsigned int i=0;i<b.length();++i){
+        if((hash&(1<<(b[i]-'A')))==0){
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main(){
 
-    string s1 = "abcdef";
-    string s2 = "bcd";
+    string s1 = "ABCDEF";
+    string s2 = "BDC";
 
-    std::cout << "Hello ..." << StringContain(s1, s2) << std::endl;
+    std::cout << "Hello ..." << StringContainBit(s1, s2) << std::endl;
 }
