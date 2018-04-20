9894318727b576cb46fabd62b77f4303da7441eb
Ryan <lifezqy@126.com>
2018-04-20 18:14:38 +0800

    strToInt

--- string.cpp
+++ string.cpp
@@ -68,10 +68,59 @@ bool StringContainBit(string &a, string &b){
     return true;
 }
 
+int StrToInt(const char *a){
+    int n = 0;
+    while(*a!=0){
+        int c = *a-'0';
+        n = n*10 + c;
+        ++a;
+    }
+    return n;
+}
+
+int StrToIntImp(const char* str){
+    static const int MAX_INT = (int)((unsigned)~0 >> 1);
+    static const int MIN_INT = -(int)((unsigned)~0 >> 1) -1;
+    unsigned int n = 0;
+
+    if(str == 0){
+        return 0;
+    }
+
+    while(isspace(*str)){
+        ++str;
+    }
+
+    int sign = 1;
+    if(*str == '+' || *str == '-'){
+        if(*str == '-'){
+            sign = -1;
+            ++str;
+        }
+    }
+
+    while(isdigit(*str)){
+        int c = *str - '0';
+        if(sign>0 && (n>MAX_INT/10 || (n==MAX_INT/10&&c>MAX_INT%10))){
+            n = MAX_INT;
+            break;
+        }else if(sign<0&&(n>(unsigned)MIN_INT/10 || (n==(unsigned)MIN_INT/10 && c>(unsigned)MIN_INT%10))){
+            n = MIN_INT;
+            break;
+        }
+
+        n = n*10+c;
+        ++str;
+    }
+
+    return sign*n;
+}
+
 int main(){
 
     string s1 = "ABCDEF";
     string s2 = "BDC";
 
     std::cout << "Hello ..." << StringContainBit(s1, s2) << std::endl;
+    std::cout << "StrToIntImp:" << StrToIntImp("12345600")*2 << std::endl;
 }
