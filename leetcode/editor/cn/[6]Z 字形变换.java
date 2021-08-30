982a110d5dd126333d98aa298ce8989224c69630
杨乾磊 <lifezqy@126.com>
2021-08-30 10:31:30 +0800

    update

---
+++ leetcode/editor/cn/[6]Z 字形变换.java
@@ -0,0 +1,95 @@
+package editor.cn;
+
+//将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。 
+//
+// 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下： 
+//
+// 
+//P   A   H   N
+//A P L S I I G
+//Y   I   R 
+//
+// 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。 
+//
+// 请你实现这个将字符串进行指定行数变换的函数： 
+//
+// 
+//string convert(string s, int numRows); 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "PAYPALISHIRING", numRows = 3
+//输出："PAHNAPLSIIGYIR"
+// 
+//示例 2：
+//
+// 
+//输入：s = "PAYPALISHIRING", numRows = 4
+//输出："PINALSIGYAHRPI"
+//解释：
+//P     I    N
+//A   L S  I G
+//Y A   H R
+//P     I
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "A", numRows = 1
+//输出："A"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 由英文字母（小写和大写）、',' 和 '.' 组成 
+// 1 <= numRows <= 1000 
+// 
+// Related Topics 字符串 
+// 👍 1249 👎 0
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution6 {
+    public static String convert(String s, int numRows) {
+        StringBuilder ret = new StringBuilder();
+
+        List<StringBuilder> cs = new ArrayList<>();
+        for (int i1 = 0; i1 < numRows; i1++) {
+            cs.add(new StringBuilder());
+        }
+
+        int row = 0;
+        boolean goingDown = false;
+        for (char c : s.toCharArray()) {
+            cs.get(row).append(c);
+            if (row == 0 || row == numRows - 1) {
+                goingDown = !goingDown;
+            }
+
+            row += goingDown ? 1 : -1;
+        }
+
+        for (StringBuilder c : cs) {
+            ret.append(c.toString());
+        }
+
+        return ret.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "PAYPALISHIRING";
+        System.out.printf("Z字型排列字符串[%s]结果为：%s\n", s, convert(s, 4));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
