a85ca342225c1e80f339c6d8ad2ce965a1284bf5
杨乾磊 <lifezqy@126.com>
2021-09-08 18:00:31 +0800

    update

--- golang/nCombine.go
+++ golang/nCombine.go
@@ -1,7 +1,17 @@
 package main
 
+import "fmt"
+
 /*
  给一个数组和参数n，返回由n 组成所有组合
+例：1, 2, 3,    参数n=2
+返回：
+3,3,
+2,3,
+2,2,
+1,3,
+1,2,
+1,1,
 */
 func nCombine(arr []int, n int) [][]int {
 
@@ -13,7 +23,7 @@ func nCombine(arr []int, n int) [][]int {
 	cur := []int{}
 	dfs1(arr, 0, n, &res, &cur)
 
-	return nil
+	return res
 }
 
 func dfs1(arr []int, idx, n int, res *[][]int, cur *[]int) {
@@ -30,14 +40,26 @@ func dfs1(arr []int, idx, n int, res *[][]int, cur *[]int) {
 		return
 	}
 
-	dfs1(arr)
+	dfs1(arr, idx+1, n, res, cur)
 
+	if len(*cur) < n {
+		*cur = append(*cur, arr[idx])
+		dfs1(arr, idx, n, res, cur)
+		*cur = (*cur)[0 : len((*cur))-1]
+	}
 }
 
 func main() {
 	arr := []int{
-		1, 2, 3, 4,
+		1, 2, 3,
 	}
 
-	nCombine(arr, 2)
+	res := nCombine(arr, 2)
+	fmt.Printf("res-----------len:%d\n", len(res))
+	for i := range res {
+		for i2 := range res[i] {
+			fmt.Print(res[i][i2], ",")
+		}
+		fmt.Println(" ")
+	}
 }
